ad7c61a816cef66895a6c79d7f8e9f2c0e576d9c
Alberto Liguori <a1liguorialberto@gmail.com>
2020-10-22 17:03:35 +0200

    adds dices game

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ for ( var i = 0; i < 100; i++){
   mailArray.push("alberto" + Math.floor(Math.random()*100)+ "@gmail.com");
   mailArray.push("gigi" + Math.floor(Math.random()*100)+ "@hotmail.it");
   mailArray.push("giacomo" + Math.floor(Math.random()*100)+ "@libero.net");
-  document.getElementById("list").innerHTML += `<li id="email-n` +[i]+ `">` + mailArray[i] + "</li>" 
+  document.getElementById("list").innerHTML += `<li id="email-n` +[i]+ `">` + mailArray[i] +`<button class="copy-email" id="button`+ [i] + `" type="button" name="button">Copy</button></li>`
 }
 
 
@@ -54,13 +54,40 @@ resetAction.addEventListener("click", function(){
   document.getElementById("userEmail").value=""
 })
 
-// copy email
+// // copy email
+//
+// var copybutton = getElementById("button")
+//
+// copybutton.addEventListener("click", function(){
+//
+//
+//
+// })
 
-var copybutton = getElementByClass("copy-email")
 
-copybutton.addEventListener("click", function(){
 
-  var textEmail = document.getElementById("button"+[i]);
-  document.getElementById("userEmail").value =textEmail;
 
-})
+// DADI
+
+
+// Generare un numero random da 1  a 6, sia per il giocatore sia per il computer
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+var player1 = Math.floor((Math.random()*6)+1)
+var player2 = Math.floor((Math.random()*6)+1)
+
+console.log(player1)
+  document.getElementById("giocata-player1").innerHTML= "il giocatore 1 ha totalizzato: "+player1
+  document.getElementById("giocata-player2").innerHTML="il giocatore 2 ha totalizzato: "+player2
+
+console.log(player2)
+
+if (player1 > player2){
+  document.getElementById("winnerIs").innerHTML = "the winner is player1 con un punteggio di " + player1
+
+} else if (player2 > player1){
+  document.getElementById("winnerIs").innerHTML = "the winner is player2 con un punteggio di " + player2
+
+} else {
+  document.getElementById("winnerIs").innerHTML = "la sfida è terminata pari"
+}
